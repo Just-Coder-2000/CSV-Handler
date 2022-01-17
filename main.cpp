cdf0dbc12112afc5c8680a9165aa829bb5abe8b6
csl <3079625093@qq.com>
2022-01-17 14:16:46 +0800

    add example for csv reading

--- main.cpp
+++ main.cpp
@@ -1,18 +1,45 @@
 #include "csvhandler.h"
 #include <artwork/geometry/point.hpp>
+#include <artwork/logger/logger.h>
+
+struct Info
+{
+    uint _gd;
+    std::string _name;
+    float _score;
+
+    Info(uint grade, const std::string &name, float score)
+        : _gd(grade), _name(name), _score(score) {}
+};
 
 int main(int argc, char const *argv[])
 {
-    using namespace ns_csv;
-    ns_csv::CSVHandler handler();
     std::ifstream file("../data/rps3f.csv");
 
-    auto ls = ns_csv::CSVHandler::read<ns_geo::RefPoint3f, uint, float, float, float>(file);
-    for (const auto &elem : ls)
+    /**
+     * @brief use macro 'CSV_READ_ITEMS' to read certain items
+     */
+    ns_log::process << "use macro 'CSV_READ_ITEMS' to read certain items" << ns_log::endl;
+    auto rps1 = CSV_READ_ITEMS(file, ',', 5, ns_geo::RefPoint3f, uint, float, float, float);
+    for (const auto &elem : rps1)
+        std::cout << elem << std::endl;
+
+    /**
+     * @brief use macro 'CSV_READ_ALL_ITEMS' to read all rest items
+     */
+    ns_log::process << "use macro 'CSV_READ_ALL_ITEMS' to read all rest items" << ns_log::endl;
+    auto rps2 = CSV_READ_ALL_ITEMS(file, ',', ns_geo::RefPoint3f, uint, float, float, float);
+    for (const auto &elem : rps2)
         std::cout << elem << std::endl;
     file.close();
 
-    // auto rps = ns_geo::RefPointSet3f::randomGenerator(10, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f);
-    // rps.write("../data/rps3f.csv", std::ios::out);
+    /**
+     * @brief use macro 'CSV_READ_FILE' to read all items in the file
+     */
+    ns_log::process << "use macro 'CSV_READ_FILE' to read all items in the file" << ns_log::endl;
+    auto info = CSV_READ_FILE("../data/data.csv", ',', Info, uint, std::string, float);
+    for (const auto &elem : info)
+        std::cout << elem._gd << ", " << elem._name << ", " << elem._score << std::endl;
+
     return 0;
 }
